17c291f46c783550049e3bd645652e1ac83a583c
Łukasz Jurczyk <zabszk@protonmail.ch>
2023-09-02 13:01:17 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Unidecode.NET;
+
+namespace UnicodeToAscii;
+
+internal static class Program
+{
+	private static void Main(string[] args)
+	{
+		try
+		{
+			string? fileName, cleanFileName;
+
+			if (args.Length == 0)
+			{
+				Console.Write("File name: ");
+				fileName = Console.ReadLine();
+			}
+			else fileName = args[0];
+
+			if (string.IsNullOrWhiteSpace(fileName))
+			{
+				Console.WriteLine("No file name provided.");
+
+				if (args.Length == 0)
+					Console.ReadKey(true);
+				return;
+			}
+
+			if (!File.Exists(fileName))
+			{
+				Console.WriteLine("File does not exist.");
+
+				if (args.Length == 0)
+					Console.ReadKey(true);
+				return;
+			}
+
+			var index = fileName.LastIndexOf('.');
+			if (index != -1)
+				cleanFileName = $"{fileName[..index]}-clean.{fileName[(index + 1)..]}";
+			else cleanFileName = fileName + "-clean";
+
+			if (File.Exists(cleanFileName))
+			{
+				Console.WriteLine($"File {cleanFileName} already exists!");
+
+				if (args.Length == 0)
+					Console.ReadKey(true);
+				return;
+			}
+
+			Console.WriteLine("Reading file...");
+			var content = File.ReadAllText(fileName);
+
+			Console.WriteLine("Writing file...");
+			File.WriteAllText(cleanFileName, content.Unidecode(), Encoding.UTF8);
+
+			Console.WriteLine("--- DONE ---");
+
+			if (args.Length == 0)
+				Console.ReadKey(true);
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("Error! Exception has occured!");
+			Console.WriteLine(e.ToString());
+			
+			if (args.Length == 0)
+				Console.ReadKey(true);
+		}
+	}
+}
